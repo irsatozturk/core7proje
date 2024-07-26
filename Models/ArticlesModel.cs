84d7453dee4b1da7f863bb87171d4dc61ca57a4a
DESKTOP-DSP8N0I\eniste <123>
2024-07-27 02:39:44 +0300

    önceki commite dahil

--- Models/ArticlesModel.cs
+++ Models/ArticlesModel.cs
@@ -8,6 +8,6 @@ namespace core7proje.Models
     public class ArticlesModel
     {
         public int Id { get; set; }
-        public string Header {get; set;}
+        public string? Header {get; set;}
     }
 }
